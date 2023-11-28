4b2351825e0b1285fa8350e9d54cabb32565d630
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2023-11-28 15:06:34 +0100

    chore: complete home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,113 @@
 import { css, cx } from "@/styled-system/css";
 import { stack } from "@/styled-system/patterns";
 import Link from "next/link";
-import { getArticles, getCollections } from "../api/queries";
+import { getArticles, getCollections, getHelpcenterSettings } from "../api/queries";
 import { ArticleIcon, CollectionIcon } from "../components/icons";
 
 export default async function Home() {
   const articles = await getArticles();
   const collections = await getCollections();
+  const settings = await getHelpcenterSettings()
+
+  const styles = {
+    sectionTitle: { 
+      fontWeight: 600, 
+      fontSize: '2xl', 
+      color: 'gray.800'
+    },
+    card: {
+      p: "16px",
+      rounded: "8px",
+      borderWidth: "1px",
+      borderColor: "gray.200",
+    },
+    cardIconContainer: {
+      w: '32px', 
+      h:'32px'
+    },
+    cardTitle: { 
+      fontWeight: 500, 
+      fontSize: "md" 
+    },
+    cardDescription: { 
+      fontSize: "sm", 
+      color: "gray.700" 
+    },
+    cardMeta: { 
+      fontSize: "xs", 
+      color: "gray.500" 
+    }
+  }
 
   return (
-    <section className={stack({ gap: "12px" })}>
+    <section className={stack({ gap: "32px" })}>
       {!!collections.length && (
-        <div className={stack({ gap: "12px" })}>
-          {collections.map((c) => (
-            <Link
-              key={c.slug}
-              href={`/collection/${c.slug}`}
-              className={cx(
-                stack({ gap: "12px", direction: 'row', align: 'center' }),
-                css({
-                  p: "16px",
-                  rounded: "8px",
-                  borderWidth: "1px",
-                  borderColor: "gray.200",
-                })
-              )}
-            >
-              <CollectionIcon />
-              <div className={cx(stack({ gap: "4px", direction: "column" }))}>
-                <h3
-                  className={cx(
-                    "category-name",
-                    css({ fontWeight: 500, fontSize: "md" })
-                  )}
-                >
-                  {c.name}
-                </h3>
-                <p
-                  className={cx(
-                    "category-description",
-                    css({ fontSize: "sm", color: "gray.500" })
-                  )}
-                >
-                  {c.description}
-                </p>
-                {!!c.blogPosts?.length && (
-                  <p
-                    className={cx(
-                      "category-articles-count",
-                      css({ fontSize: "xs", color: "gray.500" })
-                    )}
-                  >
-                    {c.blogPosts?.length} articles
-                  </p>
+        <div className={stack({ gap: '12px' })}>
+          <h1 className={cx('section-title', css(styles.sectionTitle))}>Collections</h1>
+          <div className={stack({ gap: "12px" })}>
+            {collections.map((c) => (
+              <Link
+                key={c.slug}
+                href={`/collection/${c.slug}`}
+                className={cx("collection-card",
+                  stack({ gap: "24px", direction: 'row' }),
+                  css(styles.card)
                 )}
-              </div>
-            </Link>
-          ))}
+              >
+                <div
+                  style={{ color: settings.secondary_color }}
+                  className={cx("collection-card-icon-container", css(styles.cardIconContainer))} 
+                >
+                  <CollectionIcon />
+                </div>
+                <div className={cx("collection-card-content", stack({ gap: "2px", direction: "column" }))}>
+                  <h3 className={cx("collection-card-name", css(styles.cardTitle))}>
+                    {c.name}
+                  </h3>
+                  <p className={cx("collection-card-description", css(styles.cardDescription))}>
+                    {c.description}
+                  </p>
+                  {!!c.blogPosts?.length && (
+                    <p className={cx("collection-card-article-count", css(styles.cardMeta))}>
+                      {c.blogPosts?.length} {c.blogPosts?.length > 1 ? 'articles' : 'article'}
+                    </p>
+                  )}
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
       )}
       {!!articles.length && (
-        <div className={stack({ gap: "12px" })}>
-          {articles.map((c) => (
-            <Link
-              key={c.slug}
-              href={`/article/${c.slug}`}
-              className={cx(
-                stack({ gap: "12px", direction: 'row', align: 'center' }),
-                css({
-                  p: "16px",
-                  rounded: "8px",
-                  borderWidth: "1px",
-                  borderColor: "gray.200",
-                })
-              )}
-            >
-              <div className={cx(css({w: '32px', h:'32px', color: 'gray.800'}))}>
-                <ArticleIcon  />
-              </div>
-              <div className={cx(stack({ gap: "0", direction: "column" }))}>
-                <h3
-                  className={cx(
-                    "article-card-title",
-                    css({ fontWeight: 500, fontSize: "md" })
-                  )}
+        <div className={stack({gap: '12px'})}>
+          <h1 className={cx('section-title', css(styles.sectionTitle))}>Articles</h1>
+          <div className={stack({ gap: "12px" })}>
+            {articles.map((c) => (
+              <Link
+                key={c.slug}
+                href={`/article/${c.slug}`}
+                className={cx("article-card",
+                  stack({ gap: "24px", direction: 'row' }),
+                  css(styles.card)
+                )}
+              >
+                <div
+                  style={{ color: settings.secondary_color }}
+                  className={cx("article-card-icon-container", css(styles.cardIconContainer))}
                 >
-                  {c.title}
-                </h3>
-                <p
-                  className={cx(
-                    "article-card-description",
-                    css({ fontSize: "sm", color: "gray.500" })
-                  )}
-                >
-                  {c.description}
-                </p>
-              </div>
-            </Link>
-          ))}
+                  <ArticleIcon />
+                </div>
+                <div className={cx("article-card-content", stack({ gap: "2px", direction: "column" }))}>
+                  <h3 className={cx("article-card-title", css(styles.cardTitle))}>
+                    {c.title}
+                  </h3>
+                  <p className={cx("article-card-description", css(styles.cardDescription))}>
+                    {c.description}
+                  </p>
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
       )}
     </section>
--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -111,9 +111,7 @@ export const InstagramIcon = () => {
 
 export const ArticleIcon = () => {
     return (
-      <styled.svg
-        width="576"
-        height="512"
+      <svg
         fill="none"
         viewBox="0 0 576 512"
         xmlns="http://www.w3.org/2000/svg"
@@ -122,16 +120,14 @@ export const ArticleIcon = () => {
         fill="currentColor"
         d="M249.6 471.5c10.8 3.8 22.4-4.1 22.4-15.5V78.6c0-4.2-1.6-8.4-5-11C247.4 52 202.4 32 144 32C93.5 32 46.3 45.3 18.1 56.1C6.8 60.5 0 71.7 0 83.8V454.1c0 11.9 12.8 20.2 24.1 16.5C55.6 460.1 105.5 448 144 448c33.9 0 79 14 105.6 23.5zm76.8 0C353 462 398.1 448 432 448c38.5 0 88.4 12.1 119.9 22.6c11.3 3.8 24.1-4.6 24.1-16.5V83.8c0-12.1-6.8-23.3-18.1-27.6C529.7 45.3 482.5 32 432 32c-58.4 0-103.4 20-123 35.6c-3.3 2.6-5 6.8-5 11V456c0 11.4 11.7 19.3 22.4 15.5z"
       />
-      </styled.svg>
+      </svg>
     );
   };
 
 
 export const CollectionIcon = () => {
     return (
-      <styled.svg
-        width="24"
-        height="24"
+      <svg
         fill="none"
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
@@ -140,6 +136,6 @@ export const CollectionIcon = () => {
             fill="currentColor"
             d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"
         />
-      </styled.svg>
+      </svg>
     );
   };
